3e3e270aec4aefef562ffa9d6fbe9cc1ab8f3b35
deveshwarule <deveshwarule1@gmail.com>
2024-01-29 20:37:41 +0530

    Added Connection string and register it.

---
+++ MVC Mini Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Mini_Project.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
