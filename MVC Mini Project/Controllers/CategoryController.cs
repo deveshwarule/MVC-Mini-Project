bab598490cd5e02650857a9525090b884bf81ff2
deveshwarule <deveshwarule1@gmail.com>
2024-02-01 21:10:30 +0530

    Added custom validation

--- MVC Mini Project/Controllers/CategoryController.cs
+++ MVC Mini Project/Controllers/CategoryController.cs
@@ -23,6 +23,10 @@ namespace MVC_Mini_Project.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","Dispaly Order cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
