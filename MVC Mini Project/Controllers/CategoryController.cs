a694e2d32f4434b69c55b04cad64faf74a5ee0a1
deveshwarule <deveshwarule1@gmail.com>
2024-02-01 19:26:42 +0530

    Added save method to save category.

--- MVC Mini Project/Controllers/CategoryController.cs
+++ MVC Mini Project/Controllers/CategoryController.cs
@@ -16,5 +16,16 @@ namespace MVC_Mini_Project.Controllers
             List<Category> objCategoryList=_db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index","Category");
+        }
     }
 }
