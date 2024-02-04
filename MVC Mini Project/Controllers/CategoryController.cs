653f0591112463ec981a07ecc8915a58a4152be1
deveshwarule <deveshwarule1@gmail.com>
2024-02-04 20:07:43 +0530

    Differnet ways to retrive ID

--- MVC Mini Project/Controllers/CategoryController.cs
+++ MVC Mini Project/Controllers/CategoryController.cs
@@ -34,5 +34,34 @@ namespace MVC_Mini_Project.Controllers
             }      
             return View();
         }
+        public IActionResult Edit(int? id)
+            {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+            //Category? categoryFromDB1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDB2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if(categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name","Dispaly Order cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return View();
+        }
     }
 }
